0350d04dab94179e83e8ddd6dd20d8e9dbf649ca
Saran J <saran.j@Infinitaskt.com>
2026-02-24 13:29:22 +0700

    fix nested error

--- web/homework-01/src/app/main/layout.tsx
+++ web/homework-01/src/app/main/layout.tsx
@@ -1,25 +1,16 @@
-import type { Metadata } from "next";
 import Navbar from "@/components/layout/Navbar";
-import "@/app/globals.css";
 
-export const metadata: Metadata = {
-  title: "NextJS Training",
-  description: "Homework 01",
-};
-
-export default function RootLayout({
+export default function MainLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className="antialiased font-sans">
-        <Navbar />
-        <main>
-          {children}
-        </main>
-      </body>
-    </html>
+    <>
+      <Navbar />
+      <main>
+        {children}
+      </main>
+    </>
   );
 }
